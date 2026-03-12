f8cf5f1e9b69dcc9394826031cccb4fef9e943b6
JannikStreek <Jannik.streek@gmail.com>
2026-03-12 10:43:49 +0100

    remove unused legacy packages (#147)

--- packages/oer-entities/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { OpenEducationalResource } from './open-educational-resource.entity';
-export { OerSource } from './oer-source.entity';
-export type { OerSourceStatus } from './oer-source.entity';
